f1a81a7b522759dbf36614e183b746eccf7b06e0
MarioA96 <marioaquirozs@gmail.com>
2024-04-15 15:17:11 -0600

    Añadida la validación de usuario y contraseña, y la generación de JWT
    Estos cambios mejoran la seguridad de la aplicación al asegurar que solo los usuarios con credenciales válidas puedan autenticarse y recibir un token JWT.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,38 +1,115 @@
 
 const { response } = require('express'); //Esto lo hago para cargar desde memoria el intellisense
+const bcrypt = require('bcryptjs');
+const Usuario = require('../models/Usuario');
+const { generarJWT } = require('../helpers/jwt');
 
 
-const crearUsuario = (req, res = response) => {
 
-    const { name, email, password } = req.body;
-
-    //* Hay que asegurarnos que la respuesta solo se envie una vez
-    res.status(201).json({
-        ok: true,
-        msg: 'registro',
-        name,
-        email,
-        password
-    });
-};
-
-const loginUsuario = (req, res = response) => {
+const crearUsuario = async(req, res = response) => {
 
     const { email, password } = req.body;
 
-    res.json({
-        ok: true,
-        msg: 'login',
-        email,
-        password
-    });
+    try {
+
+        let usuario = await Usuario.findOne({ email }); //Esto es para buscar un usuario en la base de datos
+        
+        if( usuario ){
+            return res.status(400).json({
+                ok: false,
+                msg: 'Un usuario existe con ese correo'
+            });
+        }
+
+        usuario = new Usuario( req.body ); //Esto es para crear una nueva instancia de usuario
+
+        // Encriptar contraseña
+        const salt = bcrypt.genSaltSync(10); //Esto es para generar un salt - para la encriptacion aleatoria
+        usuario.password = bcrypt.hashSync( password, salt ); //Esto es para encriptar la contraseña
+
+       
+        await usuario.save(); // Promesa, esto es para guardar en la base de datos
+
+
+        // Generar JWT
+        const token = await generarJWT( usuario.id, usuario.name ); //Esto es para generar el token
+
+
+        //* Hay que asegurarnos que la respuesta solo se envie una vez
+        res.status(201).json({
+            ok: true,
+            uid: usuario.id,
+            name: usuario.name,
+            token
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador'
+        });
+    }
+
+};
+
+const loginUsuario = async(req, res = response) => {
+
+    const { email, password } = req.body;
+
+    try {
+        
+        const usuario = await Usuario.findOne({ email }); //Esto es para buscar un usuario en la base de datos
+        
+        if( !usuario ){
+            return res.status(400).json({
+                ok: false,
+                msg: 'El usuario no existe con ese email'
+            });
+        }
+
+        // Confirmar los passwords
+        const validPassword = bcrypt.compareSync( password, usuario.password ); //Esto es para comparar la contraseña
+
+        if( !validPassword ){
+            return res.status(400).json({
+                ok: false,
+                msg: 'Password incorrecto'
+            });
+        }
+
+        // Generar el JWT
+        const token = await generarJWT( usuario.id, usuario.name ); //Esto es para generar el token
+
+
+        res.json({
+            ok: true,
+            uid: usuario.id,
+            name: usuario.name,
+            token
+        });
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador'
+        });
+    }
+
 }
 
-const revalidarToken = (req, res = response) => {
+const revalidarToken = async(req, res = response) => {
+
+    const { uid, name } = req;
+
+    // Generamos un nuevo JWT y lo retornamos en la respuesta/peticion
+    const token = await generarJWT( uid, name ); //Esto es para generar el token
 
     res.json({
         ok: true,
-        msg: 'renew'
+        token
     });
 };
 
